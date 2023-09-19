8db81b2afc50c45f361a615062b173de0e0c993e
andystopia <91233633+andystopia@users.noreply.github.com>
2023-09-19 17:16:46 -0400

    added vite preprocess

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,23 @@
 import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/kit/vite';
+
+
+const dev = process.argv.includes('dev');
 
 export default {
-	kit: {
-		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
-			fallback: undefined,
-			precompress: false,
-			strict: true
-		}),
+  preprocess: vitePreprocess(),
+  kit: {
+    adapter: adapter({
+      // default options are shown. On some platforms
+      // these options are set automatically — see below
+      pages: 'build',
+      assets: 'build',
+      fallback: undefined,
+      precompress: false,
+      strict: true
+    }),
     paths: {
-			base: process.env.BASE_PATH,
-		}
-	}
+      base: dev ? '' : process.env.BASE_PATH,
+    }
+  }
 };
